0e921381485d4f154270fe78911e948bbb8989a5
piyawan <tho.piyawan@gmail.com>
2018-01-04 12:56:04 +0700

    ้า้า

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -51,15 +51,6 @@ class GetMessageController extends Controller
 
 
      */
-    // public function index()
-    // {
-    //     $code = 200;
-    //     $code1 = 'pi';
-
-    //     $data = ['data' =>   $code1, 'code' => $code];
-    //     return response()->json($data);
-    // }
-
      public function getmessage()
     {         
     
@@ -81,69 +72,5 @@ class GetMessageController extends Controller
             // return $this->checkmessage($replyToken,$userMessage,$user);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
