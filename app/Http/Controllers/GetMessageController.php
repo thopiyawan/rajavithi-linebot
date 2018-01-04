fc7e8a3640e9f9bedc7bbc05530a7740c3ccc030
piyawan <tho.piyawan@gmail.com>
2018-01-04 11:24:18 +0700

    s

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use LINE\LINEBot;
