89e10a1b5908b34ae5db94225017dcf4dd2e9957
piyawan <tho.piyawan@gmail.com>
2018-01-06 14:38:12 +0700

    f

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -154,20 +154,20 @@ class GetMessageController extends Controller
 
 
 
-     public function sequents_question($seqcode)
-    {          
-                   // $question = sequents::select('question')
-                   //              ->where('seqcode',$seqcode)
-                   //              ->first();
-        $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
-        $dbconn = pg_pconnect($conn_string);
-                $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
-                while ($row = pg_fetch_row($result)) {
-                  // echo $seqcode =  $row[0];
-                     $question = $row[0];
-                }  
-                   return $question;
-    }
+    //  public function sequents_question($seqcode)
+    // {          
+    //                // $question = sequents::select('question')
+    //                //              ->where('seqcode',$seqcode)
+    //                //              ->first();
+    //     $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
+    //     $dbconn = pg_pconnect($conn_string);
+    //             $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
+    //             while ($row = pg_fetch_row($result)) {
+    //               // echo $seqcode =  $row[0];
+    //                  $question = $row[0];
+    //             }  
+    //                return $question;
+    // }
      public function sequents_question($seqcode)
     {          
                    // $question = sequents::select('question')
