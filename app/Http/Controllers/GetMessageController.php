c9989ecee84041083787949dd2892eebd2f78224
piyawan <tho.piyawan@gmail.com>
2018-01-06 14:47:10 +0700

    d

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -138,9 +138,9 @@ class GetMessageController extends Controller
                 $nextseqcode = '0002';
              
                $question = $this->sequents_question($seqcode);
+               $insert_sequentsteps = $this->insert_sequentsteps($user);
                $userMessage =  $question;
-              $insert_sequentsteps = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user}','0006','','0007','0',NOW(),NOW())") or die(pg_errormessage());
-        
+           
 
             }elseif (strpos($userMessage, 'hello') !== false || strpos($userMessage, 'สวัสดี') !== false){
                 $userMessage  = 'สวัสดีค่ะ ';
@@ -169,19 +169,13 @@ class GetMessageController extends Controller
                 }  
                    return $question;
     }
-     public function sequents_question($seqcode)
+     public function insert_sequentsteps($user)
     {          
                    // $question = sequents::select('question')
                    //              ->where('seqcode',$seqcode)
                    //              ->first();
-        $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
-        $dbconn = pg_pconnect($conn_string);
-                $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
-                while ($row = pg_fetch_row($result)) {
-                  // echo $seqcode =  $row[0];
-                     $question = $row[0];
-                }  
-                   return $question;
+        $insert_sequentsteps = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user}','0002','','0003','0',NOW(),NOW())") or die(pg_errormessage());
+        return $insert_sequentsteps;
     }
     public function replymessage($replyToken,$userMessage,$case)
     {
