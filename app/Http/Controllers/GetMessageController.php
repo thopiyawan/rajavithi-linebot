7adf3ddadca37c54ec32232ba022cb85022338e8
piyawan <tho.piyawan@gmail.com>
2018-01-06 14:25:49 +0700

    d

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -68,17 +68,18 @@ class GetMessageController extends Controller
         $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
         $dbconn = pg_pconnect($conn_string);
             
-
+          $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
            // $answer = sequents::select('question')
            //                      ->where('seqcode','0001')
            //                      ->first();
-             $seqcode = '0001';
-              $result = pg_query($dbconn,"SELECT seqcode,question FROM sequents WHERE seqcode = '$seqcode'");
-                while ($row = pg_fetch_row($result)) {
-                  echo $seqcode =  $row[0];
-                  echo $question = $row[1];
-                }   
+             // $seqcode = '0001';
+             //  $result = pg_query($dbconn,"SELECT seqcode,question FROM sequents WHERE seqcode = '$seqcode'");
+             //    while ($row = pg_fetch_row($result)) {
+             //      echo $seqcode =  $row[0];
+             //      echo $question = $row[1];
+             //    }   
 
+       $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES($user_id,'0006','','0007','0',NOW(),NOW())")
                
          
                    // return $question->question;
@@ -137,8 +138,8 @@ class GetMessageController extends Controller
                 $nextseqcode = '0002';
              
                $question = $this->sequents_question($seqcode);
-          
                $userMessage =  $question;
+        
 
             }elseif (strpos($userMessage, 'hello') !== false || strpos($userMessage, 'สวัสดี') !== false){
                 $userMessage  = 'สวัสดีค่ะ ';
@@ -153,6 +154,20 @@ class GetMessageController extends Controller
 
 
 
+     public function sequents_question($seqcode)
+    {          
+                   // $question = sequents::select('question')
+                   //              ->where('seqcode',$seqcode)
+                   //              ->first();
+        $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
+        $dbconn = pg_pconnect($conn_string);
+                $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
+                while ($row = pg_fetch_row($result)) {
+                  // echo $seqcode =  $row[0];
+                     $question = $row[0];
+                }  
+                   return $question;
+    }
      public function sequents_question($seqcode)
     {          
                    // $question = sequents::select('question')
