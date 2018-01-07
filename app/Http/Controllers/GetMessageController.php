c2c3b5bbf012309dc3d89ac24d79874c4f93a293
piyawan <tho.piyawan@gmail.com>
2018-01-07 18:53:06 +0700

    c

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -146,10 +146,9 @@ class GetMessageController extends Controller
            //$sequentsteps->seqcode
     $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
     $dbconn = pg_pconnect($conn_string);
-            $seqcode = '0004';
 
               $dbconn = pg_pconnect($conn_string);  
-          $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
+          $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '$user'");
                 while ($row = pg_fetch_object($result)) {
                   
                
@@ -269,10 +268,10 @@ class GetMessageController extends Controller
     public function seqcode_select($user){
         $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
         $dbconn = pg_pconnect($conn_string);  
-          $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
-                while ($row = pg_fetch_object($result)) {
-                   return $row->question;
-                } 
+          // $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
+          //       while ($row = pg_fetch_object($result)) {
+          //          return $row->question;
+          //       } 
 
  
 
