52cf7b87c1896f59a5d70e04b9dac817c9d90170
piyawan <tho.piyawan@gmail.com>
2018-01-08 08:44:51 +0700

    c

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -153,7 +153,7 @@ class GetMessageController extends Controller
         $conn_string = "host=ec2-54-227-247-225.compute-1.amazonaws.com port=5432 dbname=d6sqa1kjuhkplb user=kdhscmqukijgmf password=69ed8377f66479ac6222f469c6fa6cd2b2318b0ce23fd6a3f0cd7b94f18606ca";
         $dbconn = pg_pconnect($conn_string);
 
-                $result = pg_query($dbconn,"SELECT question FROM sequents WHERE seqcode = '$seqcode'");
+                $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '$user'");
                 $num = pg_num_rows($result);
                     if($num==0)         
                  {  
