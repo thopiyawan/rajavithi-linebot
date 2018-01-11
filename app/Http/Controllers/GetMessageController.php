4db7329e103a01d4b6ecfc1d5e0d19af4848596f
piyawan <tho.piyawan@gmail.com>
2018-01-11 16:51:24 +0700

    g

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -142,11 +142,11 @@ class GetMessageController extends Controller
 // $path = base_path('resources/promos/' . $folder . '/');
 // File::makeDirectory($path, 0777, true, true);
           // File::makeDirectory(public_path('document/'));
-          $path = 'public/document/';
+          //$path = 'public/document/';
           // File::makeDirectory(public_path().'/'.$path,0777,true);
-          $permissions = intval( config('permissions.directory'), 8 );
-          File::makeDirectory($path, $permissions,0777, true);
-  
+          // $permissions = intval( config('permissions.directory'), 8 );
+          // File::makeDirectory($path, $permissions,0777, true);
+            Storage::MakeDirectory(public_path('document'));
                    
      }
      public function getmessage()
