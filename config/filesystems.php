6d8e82fc2f17051dd47aa6901c3871d3d111fde6
piyawan <tho.piyawan@gmail.com>
2018-01-11 14:23:56 +0700

    g

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,14 +48,12 @@ return [
             'root' => storage_path('app'),
         ],
 
-   
-       'public' => [
-               'driver' => 'local',
-               'root'   => public_path() . '/uploads',
-               'url' => env('APP_URL').'/public',
-               'visibility' => 'public',
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
         ],
-
 
         's3' => [
             'driver' => 's3',
