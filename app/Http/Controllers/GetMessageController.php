9497d43d92ba8a1bcaa2a8b56c40ae3f361c44ae
piyawan <tho.piyawan@gmail.com>
2018-01-11 09:57:34 +0700

    x

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -509,7 +509,7 @@ if($typeMessage=='text'){
                 $update_sequentsteps = $this->update_sequentsteps($user,$seqcode,$nextseqcode);
 
                 $typedoc = '1';
-                Storage::disk('public')->put('filename', $file_content);
+                Storage::disk('public')->put('filename', $content);
                 //$save_doc = $this->save_doc($idMessage,$user,$typedoc);
                 break;
             case '0009':
