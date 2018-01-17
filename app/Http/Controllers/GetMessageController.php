ace026916e7d4f112e542790f7a16a720364ed45
piyawan <tho.piyawan@gmail.com>
2018-01-17 10:30:19 +0700

    d

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -605,7 +605,7 @@ if($typeMessage=='text'){
                    }  
                    
                     //$result = Storage::put( $fileFullSave ,$dataBinary); 
-                    Storage::disk('public')->put($fileFullSave , $dataBinary);
+                    Storage::disk('uploads')->put($fileFullSave , $dataBinary);
                  
                     // try {
                        
