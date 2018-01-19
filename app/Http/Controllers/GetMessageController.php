eb7bc744d7915e369aa6ea136cd4dc00fa239fda
piyawan <tho.piyawan@gmail.com>
2018-01-19 11:42:27 +0700

    update

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -80,7 +80,7 @@ class GetMessageController extends Controller
           $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
 
 
-          Storage::putFile('photos', new File('/path/to/photo'));
+          Storage::putFile('photos', new File('/storage'));
            // $answer = sequents::select('question')
            //                      ->where('seqcode','0001')
            //                      ->first();
