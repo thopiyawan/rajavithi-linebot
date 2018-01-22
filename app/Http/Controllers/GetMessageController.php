3dfc3c5726a93e4b93842dfdd23867145b1429f2
piyawan <tho.piyawan@gmail.com>
2018-01-22 10:25:38 +0700

    update

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -499,7 +499,7 @@ if($typeMessage=='text'){
                 $nextseqcode = '0016';
                 $question = $this->sequents_question($seqcode);
                 $userMessage = $question;
-                $case = 8; 
+                $case = 5; 
                 $update_sequentsteps = $this->update_sequentsteps($user,$seqcode,$nextseqcode);
                 $typedoc = '5';
                 break;
